0327835c2c387f0d47419f72c0c69b185cf95816
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-13 11:18:58 +0900

    Change "Hello Android" to "Hello World"

--- app/src/main/java/org/techtown/kakaotalksampling/MainActivity.java
+++ app/src/main/java/org/techtown/kakaotalksampling/MainActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        textView.setText("Hello World!")
     }
 }
