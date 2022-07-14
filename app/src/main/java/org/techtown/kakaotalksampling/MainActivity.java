355bee815783028b2cd6c50f9517f09a8496ed8f
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-14 13:53:38 +0900

    Make FriendFragment

--- app/src/main/java/org/techtown/kakaotalksampling/MainActivity.java
+++ app/src/main/java/org/techtown/kakaotalksampling/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
                             case R.id.moreTab:
                                 return true;
-                                
+
                             case R.id.scaleTab:
                                 return true;
                         }
