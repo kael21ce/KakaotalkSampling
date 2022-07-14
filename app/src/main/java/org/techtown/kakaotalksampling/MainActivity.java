98fe03d99b1a40f64f2889f7953a5fc237eb0266
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-14 13:14:40 +0900

    Make listener for bottom navigation

--- app/src/main/java/org/techtown/kakaotalksampling/MainActivity.java
+++ app/src/main/java/org/techtown/kakaotalksampling/MainActivity.java
@@ -1,9 +1,13 @@
 package org.techtown.kakaotalksampling;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.friendTab:
+                                return true;
+
+                            case R.id.chatTab:
+                                return true;
+
+                            case R.id.viewTab:
+                                return true;
+
+                            case R.id.shoppingTab:
+                                return true;
+
+                            case R.id.moreTab:
+                                return true;
+                                
+                            case R.id.scaleTab:
+                                return true;
+                        }
+                        return false;
+                    }
+
+                }
+        );
     }
 }
