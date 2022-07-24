8756d28eb19dc9aa1692e1617f9a0d1a49453750
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-25 04:09:26 +0900

    Add number-filter of getCallHistory

--- app/src/main/java/org/techtown/kakaotalksampling/ScaleFragment.java
+++ app/src/main/java/org/techtown/kakaotalksampling/ScaleFragment.java
@@ -3,6 +3,7 @@ package org.techtown.kakaotalksampling;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CallLog;
+import android.telecom.Call;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,14 +39,14 @@ public class ScaleFragment extends Fragment {
         calling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                listCalling.setText(getCallHistory());
+                listCalling.setText(getCallHistory("01029089023"));
             }
         });
 
         return v;
     }
 
-    public String getCallHistory() {
+    public String getCallHistory(String mobile) {
         String[] callSet = new String[] { CallLog.Calls.DATE, CallLog.Calls.TYPE, CallLog.Calls.NUMBER,
                 CallLog.Calls.DURATION };
         Cursor c = getActivity().getContentResolver().query(CallLog.Calls.CONTENT_URI,
@@ -55,38 +56,50 @@ public class ScaleFragment extends Fragment {
         {
             return "통화기록 없음";
         }
+
+        int recordCount = c.getCount();
         StringBuffer callBuff = new StringBuffer();
         callBuff.append("\n날짜 : 요일 : 시간 : 구분 : 전화번호 : 통화시간\n\n");
         c.moveToFirst();
-        do{
-            long callDate = c.getLong(0);
-            simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd : E요일 : HH:mm:ss");
-            String date_str = simpleDateFormat.format(new Date(callDate));
 
-            callBuff.append(date_str + " : ");
+        for (int i =0; i< recordCount; i++) {
 
-            if (c.getInt(1) == CallLog.Calls.INCOMING_TYPE)
-            {
+            String lMobile = c.getString(2);
+
+            if (lMobile.equals(mobile)) {
+
+                long callDate = c.getLong(0);
+                simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd : E요일 : HH:mm:ss");
+                String date_str = simpleDateFormat.format(new Date(callDate));
+
+                callBuff.append(date_str + " : ");
+
+                if (c.getInt(1) == CallLog.Calls.INCOMING_TYPE)
+                {
                 callBuff.append("수신 : ");
-            }
-            else if (c.getInt(1) == CallLog.Calls.OUTGOING_TYPE)
-            {
+                }
+                else if (c.getInt(1) == CallLog.Calls.OUTGOING_TYPE)
+                {
                 callBuff.append("발신 : ");
-            }
-            else if (c.getInt(1) == CallLog.Calls.MISSED_TYPE)
-            {
+                }
+                else if (c.getInt(1) == CallLog.Calls.MISSED_TYPE)
+                {
                 callBuff.append("부재중 : ");
-            }
-            else if (c.getInt(1) == CallLog.Calls.REJECTED_TYPE)
-            {
+                }
+                else if (c.getInt(1) == CallLog.Calls.REJECTED_TYPE)
+                {
                 callBuff.append("종료 : ");
+                }
+                callBuff.append(c.getString(2)+ " : ");
+                callBuff.append(c.getString(3));
+                callBuff.append("\n");
+
+                c.moveToNext();
+            } else {
+                c.moveToNext();
             }
-            callBuff.append(c.getString(2)+ " : ");
-            callBuff.append(c.getString(3));
-            callBuff.append("\n");
 
-
-        } while (c.moveToNext());
+        }
         c.close();
         return callBuff.toString();
     }
