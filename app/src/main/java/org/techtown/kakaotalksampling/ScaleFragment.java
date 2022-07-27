77abff5d31aeb1015295f2a6feb09b2d43171b4b
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-07-27 16:53:27 +0900

    Use methods of ScaleInfo class in ScaleFragment

--- app/src/main/java/org/techtown/kakaotalksampling/ScaleFragment.java
+++ app/src/main/java/org/techtown/kakaotalksampling/ScaleFragment.java
@@ -27,20 +27,22 @@ import java.util.List;
 public class ScaleFragment extends Fragment {
 
     Button calling;
-    //TextView listCalling;
     TextView incoming;
     TextView outgoing;
     TextView absContact;
     ImageView scaleHead;
+    ScaleInfo scaleInfo;
 
     SimpleDateFormat simpleDateFormat;
-    Integer numIncoming = 0;
-    Integer numOutgoing = 0;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_scale, container, false);
+
+        scaleInfo = new ScaleInfo();
+        final int[] numI = new int[1];
+        final int[] numO = new int[1];
 
         calling = v.findViewById(R.id.callCall);
         incoming = v.findViewById(R.id.incomingNum);
@@ -52,198 +54,27 @@ public class ScaleFragment extends Fragment {
         calling.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //listCalling.setText(getCallHistory("01065515413"));
-                numIncoming = getIncomingNum("01056099441");
-                numOutgoing = getOutgoingNum("01056099441");
-                incoming.setText("수신: " + numIncoming.toString());
-                outgoing.setText("발신: " + numOutgoing.toString());
-                absContact.setText("연락 횟수 차: "+betContact("01056099441"));
+                numI[0] = scaleInfo.getIncomingNum("01056099441");
+                numO[0] = scaleInfo.getOutgoingNum("01056099441");
+                incoming.setText("수신: " + numI.toString());
+                outgoing.setText("발신: " + numO.toString());
+                absContact.setText("연락 횟수 차: "+scaleInfo.betContact("01056099441"));
                 rotateScale("01056099441");
 
-                numOutgoing = 0;
-                numIncoming = 0;
             }
         });
 
         return v;
     }
 
-
-
-    //Method For Scale
-    //입력된 연락처의 정보 가져오기
-    public String getCallHistory(String mobile) {
-        String[] callSet = new String[] { CallLog.Calls.DATE, CallLog.Calls.TYPE, CallLog.Calls.NUMBER,
-                CallLog.Calls.DURATION };
-        Cursor c = getActivity().getContentResolver().query(CallLog.Calls.CONTENT_URI,
-                callSet, null, null, null);
-
-        if ( c == null)
-        {
-            return "통화기록 없음";
-        }
-
-        int recordCount = c.getCount();
-        StringBuffer callBuff = new StringBuffer();
-        callBuff.append("\n날짜 : 요일 : 시간 : 구분 : 전화번호 : 통화시간\n\n");
-        c.moveToFirst();
-
-        for (int i =0; i< recordCount; i++) {
-
-            String lMobile = c.getString(2);
-
-            if (lMobile.equals(mobile)) {
-
-                long callDate = c.getLong(0);
-                simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd : E요일 : HH:mm:ss");
-                String date_str = simpleDateFormat.format(new Date(callDate));
-
-                callBuff.append(date_str + " : ");
-
-                if (c.getInt(1) == CallLog.Calls.INCOMING_TYPE)
-                {
-                callBuff.append("수신 : ");
-                }
-                else if (c.getInt(1) == CallLog.Calls.OUTGOING_TYPE)
-                {
-                callBuff.append("발신 : ");
-                }
-                else if (c.getInt(1) == CallLog.Calls.MISSED_TYPE)
-                {
-                callBuff.append("부재중 : ");
-                }
-                else if (c.getInt(1) == CallLog.Calls.REJECTED_TYPE)
-                {
-                callBuff.append("종료 : ");
-                }
-                callBuff.append(c.getString(2)+ " : ");
-                callBuff.append(c.getString(3));
-                callBuff.append("\n");
-
-                c.moveToNext();
-            } else {
-                c.moveToNext();
-            }
-
-        }
-        c.close();
-        return callBuff.toString();
-    }
-
-    //입력된 연락처로부터 수신된 횟수 가져오기
-    public int getIncomingNum(String mobile) {
-        String[] callSet = new String[] { CallLog.Calls.DATE, CallLog.Calls.TYPE, CallLog.Calls.NUMBER,
-                CallLog.Calls.DURATION };
-        Cursor c = getActivity().getContentResolver().query(CallLog.Calls.CONTENT_URI,
-                callSet, null, null, null);
-        long now = System.currentTimeMillis();
-        long weekago = now - 604800000;
-
-        if ( c == null)
-        {
-            return 0;
-        }
-
-        int recordCount = c.getCount();
-        c.moveToFirst();
-
-        for (int i =0; i< recordCount; i++) {
-
-            String lMobile = c.getString(2);
-
-            if (lMobile.equals(mobile)) {
-                if (c.getLong(0)>=weekago) {
-                    if (Integer.parseInt(c.getString(3))>=20) {
-                        if (c.getInt(1) == CallLog.Calls.INCOMING_TYPE)
-                        {
-                            numIncoming = numIncoming + 1;
-                        }
-                        else
-                        {
-                            numIncoming = numIncoming;
-                        }
-                        c.moveToNext();
-                        } else {
-                            c.moveToNext();
-                        }
-                    }
-                }
-
-        }
-        c.close();
-        return numIncoming;
-    }
-
-
-    //입력된 연락처에게 발신한 횟수 가져오기
-    public int getOutgoingNum(String mobile) {
-        String[] callSet = new String[] { CallLog.Calls.DATE, CallLog.Calls.TYPE, CallLog.Calls.NUMBER,
-                CallLog.Calls.DURATION };
-        Cursor c = getActivity().getContentResolver().query(CallLog.Calls.CONTENT_URI,
-                callSet, null, null, null);
-
-        long now = System.currentTimeMillis();
-        long weekago = now - 604800000;
-
-        if ( c == null)
-        {
-            return 0;
-        }
-
-        int recordCount = c.getCount();
-        c.moveToFirst();
-
-        for (int i =0; i< recordCount; i++) {
-
-            String lMobile = c.getString(2);
-
-            if (lMobile.equals(mobile)) {
-                if (c.getLong(0)>=weekago) {
-                    if (Integer.parseInt(c.getString(3))>=20) {
-                        if (c.getInt(1) == CallLog.Calls.OUTGOING_TYPE)
-                        {
-                            numOutgoing = numOutgoing + 1;
-                        }
-                        else
-                        {
-                            numOutgoing = numOutgoing;
-                        }
-                        c.moveToNext();
-                        } else {
-                            c.moveToNext();
-                        }
-                    }
-            }
-
-        }
-        c.close();
-        return numOutgoing;
-    }
-
-    //입력된 연락처와의 연락 수 차이 가져오기
-    public int betContact(String mobile) {
-        numOutgoing = 0;
-        numIncoming = 0;
-
-        int numI = getIncomingNum(mobile);
-        int numO = getOutgoingNum(mobile);
-
-        return numI-numO;
-    }
-
     //저울 회전
     public void rotateScale(String mobile) {
-        int bet = betContact(mobile);
-        if (bet >=0 && bet < 10) {
+        int bet = scaleInfo.betContact(mobile);
+        if (bet<10 || bet>-10) {
             int angle = 4*bet;
             scaleHead.setRotation(angle);
-        } else if (bet >=10) {
+        } else {
             scaleHead.setRotation(45);
-        } else if (bet<0 && bet>-10) {
-            int angle = 4*bet;
-            scaleHead.setRotation(angle);
-        } else if (bet <=-10) {
-            scaleHead.setRotation(-45);
         }
     }
 
