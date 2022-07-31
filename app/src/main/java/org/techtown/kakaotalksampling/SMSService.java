9ed3556d19fe74c28024f449683e0a28464d62e1
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-08-01 01:40:10 +0900

    Create SMSService

---
+++ app/src/main/java/org/techtown/kakaotalksampling/SMSService.java
@@ -0,0 +1,47 @@
+package org.techtown.kakaotalksampling;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class SMSService extends Service {
+    public static final String STAG = "SMSService";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.i(STAG, "onCreate() 호출됨.");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.i(STAG, "onStartCommand() 호출됨.");
+
+        if (intent == null) {
+            return Service.START_STICKY;
+        } else {
+            //전화번호 가져오기
+            String number = intent.getStringExtra("number");
+            Log.i(STAG, "Given Number: " + number);
+            //전송할 인텐트 생성
+            Intent showIntent = new Intent(getApplicationContext(), ScaleFragment.class);
+            showIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|
+                    Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            showIntent.putExtra("newNumber", number);
+            startActivity(showIntent);
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+
+    public SMSService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
