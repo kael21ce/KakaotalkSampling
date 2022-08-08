a1b9d1d221293c69cd5b5e1d15d5823796fa8409
Hyeong Jun Jang <kael21ce@kaist.ac.kr>
2022-08-08 09:37:52 +0900

    Make ArrayList of Paired and near devices

--- app/src/main/java/org/techtown/kakaotalksampling/MoreFragment.java
+++ app/src/main/java/org/techtown/kakaotalksampling/MoreFragment.java
@@ -8,6 +8,7 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -26,6 +27,7 @@ import androidx.fragment.app.Fragment;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.UUID;
 
@@ -34,7 +36,15 @@ public class MoreFragment extends Fragment {
     TextView btStatus;
     Button btLED;
     BluetoothAdapter btAdapter;
-
+    Set<BluetoothDevice> pairedDevices;
+    ArrayList<String> devicePairedArrayList;
+    ArrayList<String> devicePairedNameL;
+    ArrayList<String> deviceLocalArrayList;
+    ArrayList<String> deviceLocalNameL;
+    BluetoothSocket btSocket;
+    BluetoothDevice device;
+    ConnectedThread connectedThread;
+    boolean flag;
 
 
 
@@ -49,11 +59,47 @@ public class MoreFragment extends Fragment {
             public void onClick(View v) {
                 checkBTPermission(v.getContext());
 
+                //페어링된 디바이스 목록
+                devicePairedArrayList = new ArrayList<>();
+                devicePairedNameL = new ArrayList<>();
+                if (devicePairedArrayList != null && devicePairedArrayList.isEmpty()) {
+                    devicePairedArrayList.clear();
+                }
+                pairedDevices = btAdapter.getBondedDevices();
+                if (pairedDevices.size() > 0) {
+                    for (BluetoothDevice device : pairedDevices) {
+                        String deviceName = device.getName();
+                        String deviceHardwareAddress = device.getAddress();
+                        devicePairedArrayList.add(deviceHardwareAddress);
+                        devicePairedNameL.add(deviceName);
+                    }
+                }
+
+                //주변 디바이스 목록
+                deviceLocalArrayList = new ArrayList<>();
+                deviceLocalNameL = new ArrayList<>();
+                if (btAdapter.isDiscovering()) {
+                    btAdapter.cancelDiscovery();
+                } else {
+                    if (btAdapter.isEnabled()) {
+                        btAdapter.startDiscovery();
+                        if (deviceLocalArrayList != null && !deviceLocalArrayList.isEmpty()) {
+                            deviceLocalArrayList.clear();
+                        }
+                        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                        registerReceiver(receiver, filter);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "블루투스가 켜지지 않았습니다.",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                }
+
             }
         });
         return v;
     }
 
+    //블루투스 권한 확인 및 어댑터 생성
     public void checkBTPermission(Context context) {
         btAdapter = BluetoothAdapter.getDefaultAdapter();
         if (!btAdapter.isEnabled()) {
@@ -66,6 +112,5 @@ public class MoreFragment extends Fragment {
             startActivityForResult(enableIntent, REQUEST_ENABLED_BT);
         }
     }
-
 
 }
